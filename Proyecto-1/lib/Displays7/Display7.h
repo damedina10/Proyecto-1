95a08e99713133b908ff34afda3562525c1e6424
damedina10 <89162978+damedina10@users.noreply.github.com>
2021-08-30 18:08:29 -0600

    Librería para displays
    Se creó el archivo .h para la librería de los displays de 7 segmentos.

---
+++ Proyecto-1/lib/Displays7/Display7.h
@@ -0,0 +1,14 @@
+#ifndef __DISPLAY7_H__
+#define __DISPLAY7_H__
+
+#include <Arduino.h>
+
+//función para configurar Display de 7 segmentos
+void configurardisplay(uint8_t A, uint8_t B,uint8_t C,uint8_t D,uint8_t E,uint8_t F,uint8_t G,uint8_t DP);
+
+//Función para desplegar el digito en el display
+void desplegardigito(uint8_t digito);
+
+//Función para desplegar el punto decimal en el segundo display 
+void desplegarpunto(boolean punto);
+#endif // __DISPLAY7_H__
