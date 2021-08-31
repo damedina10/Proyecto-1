bc5c4b26bd836b00bb6e3ebef550b1efbf503363
damedina10 <89162978+damedina10@users.noreply.github.com>
2021-08-30 18:19:12 -0600

    Salidas configurar displays
    Se establece que lo pines de la función configurardisplay son salidas.

--- Proyecto-1/lib/Displays7/Display7.cpp
+++ Proyecto-1/lib/Displays7/Display7.cpp
@@ -1,10 +1,23 @@
 #include "Display7.h"
 
 //función para configurar Display de 7 segmentos
-void configurardisplay(uint8_t A, uint8_t B,uint8_t C,uint8_t D,uint8_t E,uint8_t F,uint8_t G,uint8_t DP);
+void configurardisplay(uint8_t A, uint8_t B,uint8_t C,uint8_t D,uint8_t E,uint8_t F,uint8_t G,uint8_t DP){
+    pinMode(A,OUTPUT);
+    pinMode(B,OUTPUT);
+    pinMode(C,OUTPUT);
+    pinMode(D,OUTPUT);
+    pinMode(E,OUTPUT);
+    pinMode(F,OUTPUT);
+    pinMode(G,OUTPUT);
+    pinMode(DP,OUTPUT);
+}
 
 //Función para desplegar el digito en el display
-void desplegardigito(uint8_t digito);
+void desplegardigito(uint8_t digito){
+
+}
 
 //Función para desplegar el punto decimal en el segundo display 
-void desplegarpunto(boolean punto);
+void desplegarpunto(boolean punto){
+
+}
