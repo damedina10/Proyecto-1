602274d3b5557b54a3c8491e5d7bd641ea977389
damedina10 <89162978+damedina10@users.noreply.github.com>
2021-08-30 18:21:54 -0600

    Estado de las salidas configurardisplay
    Establecemos que todas las salidas estarán apagadas inicialmente.

--- Proyecto-1/lib/Displays7/Display7.cpp
+++ Proyecto-1/lib/Displays7/Display7.cpp
@@ -2,6 +2,7 @@
 
 //función para configurar Display de 7 segmentos
 void configurardisplay(uint8_t A, uint8_t B,uint8_t C,uint8_t D,uint8_t E,uint8_t F,uint8_t G,uint8_t DP){
+    //Establecemos que los pines son salidas
     pinMode(A,OUTPUT);
     pinMode(B,OUTPUT);
     pinMode(C,OUTPUT);
@@ -10,6 +11,16 @@ void configurardisplay(uint8_t A, uint8_t B,uint8_t C,uint8_t D,uint8_t E,uint8_
     pinMode(F,OUTPUT);
     pinMode(G,OUTPUT);
     pinMode(DP,OUTPUT);
+
+    //Establecemos que todas las salidas van a estar en 0 inicialmente
+    digitalWrite(A,1);
+    digitalWrite(B,1);
+    digitalWrite(C,1);
+    digitalWrite(D,1);
+    digitalWrite(E,1);
+    digitalWrite(F,1);
+    digitalWrite(G,1);
+    digitalWrite(DP,1);
 }
 
 //Función para desplegar el digito en el display
